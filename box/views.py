4104608b34ca4ea68057190248aa5b12bc17597d
v_wchhuang <v_wchhuang@tencent.com>
2022-05-14 13:37:35 +0800

    34

--- box/views.py
+++ box/views.py
@@ -5,10 +5,12 @@ from django.http import JsonResponse, HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 import db
 import blog.config_db as config_db
+from ratelimit.decorators import ratelimit
 
 
 # Create your views here.
 # blog留言
+@ratelimit(key='ip', rate='1/2s', block=True)
 def comment(requests):
     if requests.method == 'GET':
         from blog import token as tk
@@ -24,6 +26,7 @@ def comment(requests):
 
 
 # 关于博主
+@ratelimit(key='ip', rate='1/2s', block=True)
 def about(requests):
     if requests.method == 'GET':
 
@@ -31,6 +34,7 @@ def about(requests):
 
 
 # 模板
+@ratelimit(key='ip', rate='1/2s', block=True)
 def box_mode(requests):
     if requests.method == 'GET':
         from blog import token as tk
@@ -47,6 +51,7 @@ def box_mode(requests):
 
 
 # 主页
+@ratelimit(key='ip', rate='1/2s', block=True)
 def box_index(requests):
     if requests.method == 'GET':
 
@@ -67,6 +72,7 @@ def box_index(requests):
 
 
 # 存储模板传递的数据
+@ratelimit(key='ip', rate='1/2s', block=True)
 def box_content(requests):
     import datetime, time
     if requests.method == 'POST':
@@ -95,6 +101,7 @@ def box_content(requests):
 
 
 # 文章详情界面
+@ratelimit(key='ip', rate='1/2s', block=True)
 def box_show_mode(requests):
     if requests.method == "GET":
 
@@ -133,6 +140,7 @@ def box_show_mode(requests):
         return res
 
 
+@ratelimit(key='ip', rate='1/2s', block=True)
 def user_index(requests):
     # 我的界面
     if requests.method == 'GET':
@@ -160,11 +168,13 @@ def user_index(requests):
         return render(requests, "page/user_index.html", content)
 
 
+@ratelimit(key='ip', rate='1/2s', block=True)
 def login(requests):
     if requests.method == "GET":
         return render(requests, "page/login/login.html")
 
 
+@ratelimit(key='ip', rate='1/2s', block=True)
 def set_token(requests):
     # 验证登陆
     if requests.method == "POST":

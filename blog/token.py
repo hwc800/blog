51ac497cd8ab2a228daa88313138bb3362265d33
v_wchhuang <v_wchhuang@tencent.com>
2022-05-21 19:13:08 +0800

    erew

--- blog/token.py
+++ blog/token.py
@@ -7,11 +7,11 @@ import jwt
 
 def get_token(user_id, iss="wchhuang"):
     p = {
-        "exp": time.time() + 3600,
-        "iss": "%s" % iss,
-        "iat": time.time(),
+        "exp": time.time() + 3600,  # token过期时间
+        "iss": "%s" % iss,  # token鉴权者
+        "iat": time.time(),  # token开始计算的时间撮
         "data": {
-            "userid": "%s" % user_id,
+            "userid": "%s" % user_id,  # 加密信息，可自定义
         }
     }
 
@@ -20,6 +20,7 @@ def get_token(user_id, iss="wchhuang"):
 
 
 def check_token_and(token, issuer="wchhuang") -> dict:
+    """token解码"""
     result = {}
     try:
         jwt_deceode = jwt.decode(token, "123456", issuer=issuer, algorithms=["HS256"])

220ebfb1af1bf9ab260817414aa2aa96213d1249
v_wchhuang <v_wchhuang@tencent.com>
2022-05-22 00:10:01 +0800

    sss

--- box/views.py
+++ box/views.py
@@ -89,8 +89,6 @@ def box_content(requests):
         content_id = str(time.time())
         content_date = str(datetime.datetime.now())[:19]
         content = g
-        content = content.replace("\"", "\\\"")
-        content = content.replace("\'", "\\\'")
         if not db.insert(config_db.markdown_content, user_id=user_id, content=content, content_date=content_date, article_id=content_id):
             return JsonResponse({"data": "no"})
 
@@ -134,6 +132,9 @@ def box_show_mode(requests):
 
         elif "\a" in content:
             content = content.replace("\a", "\\a")
+
+        elif "\a" in content:
+            content = content.replace("#", r"\#")
 
         if not title:
             return JsonResponse({"data": "no"})
